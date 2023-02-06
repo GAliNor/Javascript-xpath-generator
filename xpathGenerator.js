40683536d079242077c9e0e56ce9bfaa69a9f5e4
ali.atguedda <ali.atguedda@gmail.com>
2023-02-07 00:29:11 +0100

    exception handling: return null in case of exception

--- xpathGenerator.js
+++ xpathGenerator.js
@@ -5,14 +5,18 @@
  * @returns { string } - xpath value
  */
 export const generateXPath = (domElement) => {
-    if(domElement.id.length) {
-        return `//*[@id='${domElement.id}']`;
-    } else if (document.getElementsByTagName(domElement.tagName).length === 1) {
-        return `//${domElement.tagName.toLowerCase()}`;
-    } else {
-        const nodesInSameLevel = Array.from(domElement.parentNode.childNodes);
-        const nodesInSameLevelWithSameTagName = nodesInSameLevel.filter(element => element.tagName === domElement.tagName);
-        const domElementLevelPosition = nodesInSameLevelWithSameTagName.indexOf(domElement);
-        return `${generateXPath(domElement.parentNode)}/${domElement.tagName.toLowerCase()}${domElementLevelPosition ? `[${domElementLevelPosition + 1}]` : ''}`;
+    try {
+        if(domElement.id.length) {
+            return `//*[@id='${domElement.id}']`;
+        } else if (document.getElementsByTagName(domElement.tagName).length === 1) {
+            return `//${domElement.tagName.toLowerCase()}`;
+        } else {
+            const nodesInSameLevel = Array.from(domElement.parentNode.childNodes);
+            const nodesInSameLevelWithSameTagName = nodesInSameLevel.filter(element => element.tagName === domElement.tagName);
+            const domElementLevelPosition = nodesInSameLevelWithSameTagName.indexOf(domElement);
+            return `${generateXPath(domElement.parentNode)}/${domElement.tagName.toLowerCase()}${domElementLevelPosition ? `[${domElementLevelPosition + 1}]` : ''}`;
+        }
+    } catch {
+        return null;
     }
 }
