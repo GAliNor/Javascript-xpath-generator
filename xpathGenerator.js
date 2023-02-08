c4d6aa96465c097622de7b941b7ff2bafeb2c758
GAliNor <53081485+GAliNor@users.noreply.github.com>
2023-02-08 09:35:27 +0100

    Code improvement

--- xpathGenerator.js
+++ xpathGenerator.js
@@ -4,19 +4,15 @@
  * @param { HTMLElement } domElement - DOM element for xpath generation
  * @returns { string } - xpath value
  */
-export const generateXPath = (domElement) => {
-    try {
-        if(domElement.id.length) {
-            return `//*[@id='${domElement.id}']`;
-        } else if (document.getElementsByTagName(domElement.tagName).length === 1) {
-            return `//${domElement.tagName.toLowerCase()}`;
-        } else {
-            const nodesInSameLevel = Array.from(domElement.parentNode.childNodes);
-            const nodesInSameLevelWithSameTagName = nodesInSameLevel.filter(element => element.tagName === domElement.tagName);
-            const domElementLevelPosition = nodesInSameLevelWithSameTagName.indexOf(domElement);
-            return `${generateXPath(domElement.parentNode)}/${domElement.tagName.toLowerCase()}${domElementLevelPosition ? `[${domElementLevelPosition + 1}]` : ''}`;
-        }
-    } catch {
-        return null;
+const generateXPath = (domElement) => {
+    if(domElement.id.length) {
+        return `//*[@id='${domElement.id}']`;
     }
+    if (document.getElementsByTagName(domElement.tagName).length === 1) {
+        return `//${domElement.tagName.toLowerCase()}`;
+    }
+    const nodesInSameLevel = Array.from(domElement.parentNode.childNodes);
+    const nodesInSameLevelWithSameTagName = nodesInSameLevel.filter(element => element.tagName === domElement.tagName);
+    const domElementLevelPosition = nodesInSameLevelWithSameTagName.indexOf(domElement);
+    return `${generateXPath(domElement.parentNode)}/${domElement.tagName.toLowerCase()}${`[${domElementLevelPosition + 1}]`}`;
 }
