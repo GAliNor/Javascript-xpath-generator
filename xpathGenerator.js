c971274f5dc42ec6fcbc94f5505a840c304d5f5a
atguedda <atguedda@gmail.com>
2023-02-10 19:27:26 +0100

    add full xpath generation support

--- xpathGenerator.js
+++ xpathGenerator.js
@@ -4,15 +4,18 @@
  * @param { HTMLElement } domElement - DOM element for xpath generation
  * @returns { string } - xpath value
  */
-const generateXPath = (domElement) => {
-    if(domElement.id.length) {
+const generateXPath = (domElement, full=false) => {
+    if(!full && domElement.id.length) {
         return `//*[@id='${domElement.id}']`;
     }
-    if (document.getElementsByTagName(domElement.tagName).length === 1) {
+    if (!full && document.getElementsByTagName(domElement.tagName).length === 1) {
         return `//${domElement.tagName.toLowerCase()}`;
+    }
+    if(full && domElement.tagName.toLowerCase() === 'html') {
+        return '/html'
     }
     const nodesInSameLevel = Array.from(domElement.parentNode.childNodes);
     const nodesInSameLevelWithSameTagName = nodesInSameLevel.filter(element => element.tagName === domElement.tagName);
     const domElementLevelPosition = nodesInSameLevelWithSameTagName.indexOf(domElement);
-    return `${generateXPath(domElement.parentNode)}/${domElement.tagName.toLowerCase()}${`[${domElementLevelPosition + 1}]`}`;
+    return `${generateXPath(domElement.parentNode, full)}/${domElement.tagName.toLowerCase()}${`[${domElementLevelPosition + 1}]`}`;
 }
